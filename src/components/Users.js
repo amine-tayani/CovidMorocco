f3e750e8d17b83c6c39a184560366737646ea381
maximaCoder <62437851+maximaCoder@users.noreply.github.com>
2020-06-29 00:35:14 +0100

    more ...

--- src/components/Users.js
+++ src/components/Users.js
@@ -39,55 +39,45 @@ class Users extends Component {
       )
     }
     return (
-      <section className="text-gray-700 body-font ">
-        <div className="container px-5 py-24 mx-auto animated ">
-          <div className="flex flex-col text-center w-full mb-20">
-            <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">Corona Virus Statistics</h2>
-            <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">Covid-19 {global.country} Situation</h1>
+      <section class="text-gray-700 body-font">
+        <div class="container px-5 py-24 mx-auto animated">
+          <div class="flex flex-col text-center w-full mb-20">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Covid-19 {global.country} Situation</h1>
+            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+              Follow the latest developments in figures of the Covid-19 disease pandemic linked to the new coronavirus live.</p>
           </div>
-          <div className="flex flex-wrap -m-4 ">
-            <div className="p-4 md:w-1/3">
-              <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col shadow-lg">
-                <div className="flex items-center justify-center mb-3">
-                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center text-white flex-shrink-0">
-                    <img className="h-8" alt="" src="https://cdn.iconscout.com/icon/free/png-64/corona-virus-2349959-1956094.png" ></img>
-                  </div>
-                  <h2 className="text-gray-900 text-lg title-font font-medium">Total Cases</h2>
-                </div>
-                <div className="flex-grow">
-                  <h2 className="text-center title-font font-medium text-3xl text-gray-900">{global.confirmed}</h2>
-                </div>
+          <div class="flex flex-wrap -m-4 text-center">
+            <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+              <div class="shadow-lg border-2 border-gray-200 px-4 py-6 rounded-lg">
+                <img className="w-12 h-12 mb-3 inline-block" alt="" src="https://cdn.iconscout.com/icon/free/png-64/corona-virus-2349959-1956094.png" ></img>
+                <h2 class="title-font font-medium text-3xl text-gray-900">{global.confirmed}</h2>
+                <p class="leading-relaxed">Total Cases</p>
               </div>
             </div>
-            <div className="p-4 md:w-1/3 ">
-              <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col shadow-lg">
-                <div className="flex items-center justify-center mb-3">
-                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center text-white flex-shrink-0">
-                    <img alt="" className="h-8" src="https://cdn.iconscout.com/icon/free/png-64/health-insurance-1657882-1406824.png" ></img>
-                  </div>
-                  <h2 className="text-gray-900 text-lg title-font font-medium">Total Recovered</h2>
-                </div>
-                <div className="flex-grow">
-                  <h2 className="text-center title-font font-medium text-3xl text-gray-900">{global.recovered}</h2>
-                </div>
+            <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+              <div class="shadow-lg border-2 border-gray-200 px-4 py-6 rounded-lg">
+                <img alt="" className="w-12 h-12 mb-3 inline-block" src="https://cdn.iconscout.com/icon/free/png-64/health-insurance-1657882-1406824.png" ></img>
+                <h2 class="title-font font-medium text-3xl text-gray-900">{global.recovered}</h2>
+                <p class="leading-relaxed">Total Recovered</p>
               </div>
             </div>
-            <div className="p-4 md:w-1/3">
-              <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col shadow-lg">
-                <div className="flex items-center justify-center mb-3">
-                  <div className="w-8 h-8 mr-3 inline-flex items-center justify-center text-white flex-shrink-0">
-                    <img alt="" className="h-8" src="https://cdn.iconscout.com/icon/free/png-64/funeral-death-gravestone-halloween-rip-grave-3-31643.png" ></img>
-                  </div>
-                  <h2 className="text-gray-900 text-lg title-font font-medium">Total Deaths</h2>
-                </div>
-                <div className="flex-grow">
-                  <h2 className="text-center title-font font-medium text-3xl text-gray-900">{global.deaths}</h2>
-                </div>
+            <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+              <div class="shadow-lg border-2 border-gray-200 px-4 py-6 rounded-lg">
+                <img alt="" className="w-12 h-12 mb-3 inline-block" src="https://cdn.iconscout.com/icon/free/png-64/bones-1721930-1466048.png" ></img>
+                <h2 class="title-font font-medium text-3xl text-gray-900">{global.critical}</h2>
+                <p class="leading-relaxed">Critical cases</p>
+              </div>
+            </div>
+            <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+              <div class="shadow-lg border-2 border-gray-200 px-4 py-6 rounded-lg">
+                <img alt="" className="w-12 h-12 mb-3 inline-block" src="https://cdn.iconscout.com/icon/free/png-64/funeral-death-gravestone-halloween-rip-grave-3-31643.png" ></img>
+                <h2 class="title-font font-medium text-3xl text-gray-900">{global.deaths}</h2>
+                <p class="leading-relaxed">Total Deaths</p>
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </section >
     );
   }
 }
